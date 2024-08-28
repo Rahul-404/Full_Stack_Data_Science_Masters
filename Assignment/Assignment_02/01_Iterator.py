4a68d28abaea6869ab63ea8cae0736465c81752b
Rahul-404 <rahulshelke981@gmail.com>
2024-08-28 13:55:36 +0530

    added: Q4 hands on python script

---
+++ Assignment/Assignment_02/01_Iterator.py
@@ -0,0 +1,20 @@
+class MyIterator:
+    def __init__(self, start, end) -> None:
+        self.current = start
+        self.end = end
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current > self.end:
+            raise StopIteration
+        else:
+            self.current += 1
+            return self.current - 1
+
+# using iterator
+my_iter = MyIterator(1, 5)
+
+for value in my_iter:
+    print(value)
