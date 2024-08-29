0561a1ce9abda2f0a4903bc617dd1be1ad677068
Rahul-404 <rahulshelke981@gmail.com>
2024-08-29 20:53:49 +0530

    added: Q1 hands on python script

---
+++ Assignment/02_OOPS/1_Inheritance.py
@@ -0,0 +1,21 @@
+# Base class
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def introduce(self):
+        print(f"Hi, I'm {self.name} and I'm {self.age} years old.")
+
+# Derived class
+class Student(Person):
+    def __init__(self, name, age, student_id):
+        super().__init__(name, age) # Call the constructor of the base
+        self.student_id = student_id
+
+    def introduce(self):
+        super().introduce() # Calling the introduce method of base class
+        print(f"My student ID is {self.student_id}")
+
+obj = Student("Rahul", 25, 65)
+obj.introduce()
