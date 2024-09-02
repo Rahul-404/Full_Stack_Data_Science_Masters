50507c595b31b0c6cc32a1b6583896753a3de291
Rahul-404 <rahulshelke981@gmail.com>
2024-09-02 14:20:05 +0530

    added: Q9 hands on python script

---
+++ Assignment/Assignment_02_Hands_on/09_read_file_line_by_line.py
@@ -0,0 +1,25 @@
+# create a sample file for demonstration
+example_file_content = """Line 1
+Line 2
+Line 3
+Line 4
+Line 5"""
+
+with open("example_file.txt", 'w') as file:
+    file.write(example_file_content)
+
+
+def read_file_line_by_line(file_path):
+    """
+    A generator function that yields lines from a file one by one.
+    """
+    with open(file_path, 'r') as file:
+        for line in file:
+            yield line.strip() # yield each line, stripped of training newline characters
+
+
+# example usage
+file_path  = "example_file.txt"
+
+for line in read_file_line_by_line(file_path):
+    print(line)
